7b77c7b00b4f6da6f9904e7a620e4a8de39e64c8
Kelvin <nguyen007@e.ntu.edu.sg>
2021-10-17 19:00:23 +0800

    🎉 Update home page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <div class="p-1 mb-1 bg-success text-white"></div>
+      <div className="p-1 mb-1 bg-success text-white"></div>
       <header className="App-header">
         <img
           src="https://github.com/pebabion.png"
@@ -23,13 +23,21 @@ function App() {
             (Kelvin Nguyen Le)
           </strong>
         </h2>
-        <div class="p-2 mb-1"></div>
+        <div className="p-2 mb-1"></div>
         <p>I'm a Data Engineer 👨‍🔧🔧⚙️🚀</p>
         <p>
           I am currently living in Singapore 🇸🇬, a sunny tropical island 🌤🏖🌴 in
           Southeast Asia 🌏
         </p>
+        <p>
+          I enjoy building applications/platforms with distributed systems,
+          especially for AI and ML.
+          <br />
+          Feel free to head over to my GitHub for the projects that I am working
+          on! 🔨🛠
+        </p>
       </main>
+      <div className="p-2 mb-1"></div>
       <Footer></Footer>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class="footer fixed-bottom py-2 my-2 text-muted fs-6">
+    <footer className="footer py-2 my-2 text-muted fs-6">
       © All rights reserved. And all that stuff 👻
     </footer>
   );
