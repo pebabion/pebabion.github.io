737a573d66547d9befbca3ead6c92262b4fbb551
pebabion <nguyen007@e.ntu.edu.sg>
2022-10-30 01:05:05 +0800

    chore: bunch of margins and paddings

--- src/App.js
+++ src/App.js
@@ -9,12 +9,10 @@ function App() {
   const { currentMode } = useStateContext();
   return (
     <div className={currentMode === "Dark" ? "dark" : ""}>
-      <div className="text-center dark:bg-main-dark-bg">
+      <div className="flex flex-col min-h-screen text-center dark:bg-main-dark-bg">
         <div className="h-2 bg-green-600" />
-        <div className="flex flex-col h-screen">
-          <div className="mb-auto">
-            <Introduction />
-          </div>
+        <Introduction />
+        <div className="mt-auto m-5">
           <Footer />
         </div>
       </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="text-sm m-5 text-center text-gray-600 dark:text-gray-200">
+    <footer className="text-sm text-center text-gray-600 dark:text-gray-200">
       © All rights reserved. And all that stuff 👻
     </footer>
   );
